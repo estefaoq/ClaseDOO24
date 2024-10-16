21c349cb4b311bcb81faa4b9878525c2d4d94a70
estefaoq <140673103+estefaoq@users.noreply.github.com>
2024-10-15 23:57:14 -0500

    clase 15 oct

--- CrossCutting/src/co/edu/uco/crosscutting/helpers/SqlConnectionHelper.java
+++ CrossCutting/src/co/edu/uco/crosscutting/helpers/SqlConnectionHelper.java
@@ -1,5 +1,21 @@
 package co.edu.uco.crosscutting.helpers;
 
-public class SqlConnectionHelper {
+public final  class SqlConnectionHelper {
+	private SqlConnectionHelper() {
+		
+	}
+	public static boolean connectionIsNull(final Connection connection) {
+		return ObjectHelper.isNull(connection);
+		
+	}
+	public static boolean connectionIsOpen(final Connection connection) {
+		try {
+		return !connectionIsNull(connection) && !connection.isClosed();
+		}catch(final SQLException e) {
+		   var userMessage = "se ha presentado un problema inesperado tratando de" + "llevar a cabo la validacion de si la conexion" + "estaba abierta o no abierta. porfavor revise el log de errores para" + "mas detalle del error presentado...";
+		   throw new UcoApplicationException(userMessage, technicalMessage, exception, Layer.DATA);
+		}
+	}
+	
 
 }
--- ucobet/src/main/java/co/edu/uco/ucobet/data/dao/impl/sql/sqlserver/SqlDAO.java
+++ ucobet/src/main/java/co/edu/uco/ucobet/data/dao/impl/sql/sqlserver/SqlDAO.java
@@ -1,5 +1,40 @@
 package co.edu.uco.ucobet.data.dao.impl.sql.sqlserver;
+import java.sql.Connection;
+import java.util.List;
+import java.util.UUID;
+
+import co.edu.uco.crosscutting.exception.DataUcoBetException;
+import co.edu.uco.ucobet.data.dao.CountryDAO;
+import co.edu.uco.ucobet.entity.CountryEntity;
 
 public class SqlDAO {
+	//Good
+	
+	private Connection connection;
+	
+	protected SqlDAO (final Connection connection) {
+		setConnection(connection);
+	}
+	
+	
+	
+	protected Connection getConnection() {
+		return connection;
+	}
+
+	private void setConnection(final Connection connection) {
+		validateIfConnectionIsOpen(connection);		
+		this.connection = connection;
+	}
+	
+	private void validateIfConnectionIsOpen(final Connection connection) {
+		if(!SqlConnectionHelper.connectionIsOpen(connection)) {
+			var userMessage = "Se ha presentado un problema inesperado, tratando de llevar a cabo la operación deseada...";
+			var technicalMessage = "No es posible crear un acceso a datos de tipo sql con una conexión nula o cerrada...";
+			
+			throw DataUcoBetException.crear(userMessage, technicalMessage);
+		}
+	}
+	
 
 }
